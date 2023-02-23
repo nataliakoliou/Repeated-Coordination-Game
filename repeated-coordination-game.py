b29204c447820a77fced5e21c2787fad6c24db01
nataliakoliou <83582927+nataliakoliou@users.noreply.github.com>
2023-02-23 12:54:57 +0200

    final.version#2

--- repeated-coordination-game.py
+++ repeated-coordination-game.py
@@ -4,24 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Iterative Coordination Game
-actions = ["Action[1]", "Action[2]"]
-types = ["X", "X", "X", "Y", "Y", "Y", "Y"]
-payoff = {"X": [[2,0],[0,1]], "Y": [[1,0],[0,2]]}
+actions = ["A1", "A2"]
+types = ["X", "X", "X", "X", "Y", "Y", "Y"]
+payoff = [[1,0],[0,1]]
 neighbors = [[0,4],[0,5],[0,3],[1,2],[1,4],[2,3],[4,5],[5,6]]
-frequencies = [sum(num in sublist for sublist in neighbors) for num in range(max(max(sublist) for sublist in neighbors) + 1)]
+frequencies = [sum(num in sublist for sublist in neighbors) for num in range(max(max(sublist) for sublist in neighbors) + 1)]  # [0,1,2,3,4,5,6] ~ [3,2,2,2,3,3,1]
 
-def game_print(phase, ep, maxep, players_data):
+def game_print(phase, ep, m, episodes, players_data, pair):
 
-    if phase == "exploration" and ep % int(maxep/10) == 0:
-        print("\nRound.{} - Exploration ".format(ep) + "*"*55)
+    if phase == "exploration" and m == 10 and ep % int(episodes/4) == 0:
+        print("\n" + "*"*50 + '\033[1m' + "\nExploration:" + '\033[0m' + " (pair: {}, episode: {})".format(pair, ep))
         for p in range(len(players_data)):
-            print(players_data[p]["name"] + ": (action: " + players_data[p]["action"] + ", q-table: " + str(players_data[p].get("qtable")) + ")")
+            print('\033[1m' + players_data[p]["name"] + ":" + '\033[0m' + " {type: " + players_data[p]["type"] + \
+            ", preference: " + players_data[p]["preference"] + ", action: " + str(players_data[p]["action"]) + "}")
         time.sleep(0)
 
-    elif phase == "exploitation" and (ep % int(maxep/10) == 0 or (ep >= maxep-10 and ep <= maxep)):
-        print("\nRound.{} - Exploitation ".format(ep) + "*"*55)
+    elif phase == "exploitation" and m == 10 and ep % int(episodes/4) == 0:
+        print("\n" + "*"*50 + '\033[1m' + "\nExploitation:" + '\033[0m' + " (pair: {}, episode: {})".format(pair, ep))
         for p in range(len(players_data)):
-            print(players_data[p]["name"] + ": (action: " + players_data[p]["action"] + ", q-table: " + str(players_data[p].get("qtable")) + ")")
+            print('\033[1m' + players_data[p]["name"] + ":" + '\033[0m' + " {type: " + players_data[p]["type"] + \
+            ", preference: " + players_data[p]["preference"] + ", action: " + str(players_data[p]["action"]) + "}")
         time.sleep(0)
 
 def graph(x_max, y_ranges, x_label, y_label):
@@ -48,45 +50,41 @@ def string_to_id(string, list):
 def qlearning(players_data):
     moments = 10
     episodes = 2500
-    ep = 0
     alpha = 0.1
     gamma = 0.9
     epsilon = 1.0
     decay = 0.01
     accuracy = 10
 
-    rwXep = [[0 for e in range(episodes)] for p in range(len(players_data))]
     acXep = [[0 for e in range(episodes)] for p in range(len(players_data))]
+    rwXep = [[0 for e in range(episodes)] for p in range(len(players_data))]
     mrwXep = [[0 for e in range(episodes)] for t in range(len(set(types)))]
 
+    ep = 0  # episodes counter
     while ep < episodes: 
-        for pair in neighbors:  # n = [0,4], ...
-            m = 0
+        for pair in neighbors:
+            m = 0  # moments counter
             choice = round(random.uniform(0, 1), accuracy)
             while m <= moments:
                 m += 1
-                if choice < epsilon : # EXPLORATION
+                if choice < epsilon :  # EXPLORATION
                     players_data = explore(pair, players_data)
                     acXep, rwXep, mrwXep, players_data = update(pair, players_data, alpha, gamma, acXep, rwXep, mrwXep, ep, moments)
-                    #game_print("exploration", ep, episodes, players_data)
-                else: # EXPLOITATION
+                    game_print("exploration", ep, m, episodes, players_data, pair)
+                else:  # EXPLOITATION
                     players_data = exploit(pair, players_data)
                     acXep, rwXep, mrwXep, players_data = update(pair, players_data, alpha, gamma, acXep, rwXep, mrwXep, ep, moments)
-                    #game_print("exploitation", ep, episodes, players_data)
+                    game_print("exploitation", ep, m, episodes, players_data, pair)
         ep = ep + 1
         if epsilon > 0.000000:
-            epsilon -= decay/20  # X=20
+            epsilon -= decay/20  # epsilon is reduced by decay every 20 episodes
         else:
             epsilon = 0.000000
 
-    x_axis = "Episode"
-    y_axis = "Player's Choice"
     acXep = [[int(num) for num in sublist] for sublist in acXep]
-    graph(episodes, acXep, x_axis, y_axis)
-    y_axis = "Player's Reward"
-    graph(episodes, rwXep, x_axis, y_axis)
-    y_axis = "Player-Types Mean Reward"
-    graph(episodes, mrwXep, x_axis, y_axis)
+    graph(episodes, acXep, "Episode", "Player's Choice")
+    graph(episodes, rwXep, "Episode", "Player's Reward")
+    graph(episodes, mrwXep, "Episode", "Player-Types Mean Reward")
 
 def update(pair, players_data, alpha, gamma, acXep, rwXep, mrwXep, ep, moments):
     for p in pair:
@@ -102,7 +100,7 @@ def update(pair, players_data, alpha, gamma, acXep, rwXep, mrwXep, ep, moments):
         rwXep[p][ep] += reward/moments
         mrwXep[string_to_id(player_type, list(set(types)))][ep] += reward/(moments*types.count(player_type))
         acXep[p][ep] += string_to_id(player_action, actions)/(moments*frequencies[p])   # this value will finally
-        # be something like e.x: 0.8 indicating that this player chose Action-2 (8) times in this current episode
+        # be something like e.x: 0.8 indicating that this player chose A2 (8) times in this current episode
     return acXep, rwXep, mrwXep, players_data
 
 def get_opponent(player, pair):
@@ -110,9 +108,13 @@ def get_opponent(player, pair):
     return opponent
 
 def explore(pair, players_data):
-    for p in pair:  # [0,4]
-        random_action = random.choice([actions[0], actions[1]])
-        players_data[p]["action"] = random_action
+    for p in pair:
+        prob = random.uniform(0, 1)
+        if prob < 0.6:
+            players_data[p]["action"] = players_data[p]["preference"]
+        else:
+            random_action = random.choice([actions[0], actions[1]])
+            players_data[p]["action"] = random_action
     return players_data
 
 def exploit(pair, players_data):
@@ -130,14 +132,17 @@ def main():
             "name": "Player[{}]".format(p),
             "type": types[p],
             "action": None,
-            "payoff": payoff[types[p]],
+            "preference": actions[0] if types[p] == "X" else actions[1],
+            "payoff": payoff,
             "qtable": {actions[0]: 0, actions[1]: 0}
             })
 
     qlearning(players_data)
 
+    print('\033[1m' + "Nash Equilibrium Strategies: " + '\033[0m')
     for p in range(num_players):
-        print(players_data[p]["name"] + ": (action: " + players_data[p]["action"] + ", q-table: " + str(players_data[p].get("qtable")) + ")")
+        print('\033[1m' + players_data[p]["name"] + ":" + '\033[0m' + " {type: " + players_data[p]["type"] + \
+        ", preference: " + players_data[p]["preference"] + ", action: " + players_data[p]["action"] + "}")
        
 if __name__ == "__main__":
     main()
